1674bfdce156b9bed77303b966744d3f1005ca24
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-02 03:13:52 +0500

    Add route for the Greeting component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React World</h1>
+      <h1>Welcome to React Front-End World</h1>
+      <Routes>
+        <Route path='/' element={<Greeting />} />
+      </Routes>
     </div>
   );
 }
